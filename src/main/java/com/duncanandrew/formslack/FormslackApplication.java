8f0c9d477b79b9a578695f34e2a45c4080e0434f
Duncan Andrew <duncan.andrew@avanade.com>
2024-02-27 11:20:37 -0800

    initial commit

---
+++ src/main/java/com/duncanandrew/formslack/FormslackApplication.java
@@ -0,0 +1,13 @@
+package com.duncanandrew.formslack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormslackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormslackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duncanandrew/formslack/FormslackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duncanandrew.formslack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormslackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
